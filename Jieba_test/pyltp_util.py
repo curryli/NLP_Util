e4e3ab837c5ead60c953fdccf065a1c5b97a4ff6
lixurui <xurui.lee@msn.com>
2018-02-06 20:26:06 +0800

    textrank

---
+++ Jieba_test/pyltp_util.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import os
+from pyltp import SentenceSplitter
+from pyltp import Segmentor
+from pyltp import Postagger
+from pyltp import NamedEntityRecognizer
+from pyltp import Parser
+from pyltp import SementicRoleLabeller
+
+
+
+
+LTP_DATA_DIR = 'D:\Libs_for_All\NLP_Libs\ltp_data_v3.3.1'  # ltp模型目录的路径
+cws_model_path = os.path.join(LTP_DATA_DIR, 'cws.model')  # 分词模型路径，模型名称为`cws.model`
+pos_model_path = os.path.join(LTP_DATA_DIR, 'pos.model')  # 词性标注模型路径，模型名称为`pos.model`
+ner_model_path = os.path.join(LTP_DATA_DIR, 'ner.model')  # 命名实体识别模型路径，模型名称为`ner.model`
+par_model_path = os.path.join(LTP_DATA_DIR, 'parser.model')  # 依存句法分析模型路径，模型名称为`parser.model`
+srl_model_path = os.path.join(LTP_DATA_DIR, 'srl')  # 语义角色标注模型目录路径，模型目录为`srl`。注意该模型路径是一个目录，而不是一个文件。
+
+
+#分句，也就是将一片文本分割为独立的句子
+def sentence_splitter(sentence='你好，你觉得这个例子从哪里来的？当然还是直接复制官方文档，然后改了下这里得到的。我的微博是MebiuW，转载请注明来自MebiuW！'):
+    sents = SentenceSplitter.split(sentence)  # 分句
+    return sents
+
+#分词
+def segmentor(sentence='你好，你觉得这个例子从哪里来的？当然还是直接复制官方文档，然后改了下这里得到的。我的微博是MebiuW，转载请注明来自MebiuW！'):
+    segmentor = Segmentor()  # 初始化实例
+    segmentor.load(cws_model_path)  # 加载模型
+    words = segmentor.segment(sentence)  # 分词
+    segmentor.release()  # 释放模型
+    return list(words)
+
+def posttagger(words):
+    postagger = Postagger() # 初始化实例
+    postagger.load(pos_model_path)  # 加载模型
+    postags = postagger.postag(words)  # 词性标注
+    postagger.release()  # 释放模型
+    return postags
+
+#命名实体识别
+def ner(words, postags):
+    recognizer = NamedEntityRecognizer() # 初始化实例
+    recognizer.load(ner_model_path)  # 加载模型
+    netags = recognizer.recognize(words, postags)  # 命名实体识别
+    recognizer.release()  # 释放模型
+    return netags
+
+#依存语义分析
+def parse(words, postags):
+    parser = Parser() # 初始化实例
+    parser.load(par_model_path)  # 加载模型
+    arcs = parser.parse(words, postags)  # 句法分析
+    parser.release()  # 释放模型
+    return arcs
+
+#角色标注
+def role_label(words, postags, netags, arcs):
+    labeller = SementicRoleLabeller() # 初始化实例
+    labeller.load(srl_model_path)  # 加载模型
+    roles = labeller.label(words, postags, netags, arcs)  # 语义角色标注
+    labeller.release()  # 释放模型
+    return roles
---
+++ Jieba_test/simple_TextRank.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, unicode_literals
+import sys
+from operator import itemgetter
+from collections import defaultdict
+import jieba.posseg
+import os
+
+import pyltp_util
+
+# class UndirectWeightedGraph:
+#     d = 0.85
+#
+#     def __init__(self):
+#         self.graph = defaultdict(list)
+#
+#     def addEdge(self, start, end, weight):
+#         # use a tuple (start, end, weight) instead of a Edge object
+#         self.graph[start].append((start, end, weight))
+#         self.graph[end].append((end, start, weight))
+#
+#     def rank(self):
+#         ws = defaultdict(float)
+#         outSum = defaultdict(float)
+#
+#         wsdef = 1.0 / (len(self.graph) or 1.0)
+#         for n, out in self.graph.items():
+#             ws[n] = wsdef
+#             outSum[n] = sum((e[2] for e in out), 0.0)
+#
+#         # this line for build stable iteration
+#         sorted_keys = sorted(self.graph.keys())
+#         for x in xrange(10):  # 10 iters
+#             for n in sorted_keys:
+#                 s = 0
+#                 for e in self.graph[n]:
+#                     s += e[2] / outSum[e[1]] * ws[e[1]]
+#                 ws[n] = (1 - self.d) + self.d * s
+#
+#         (min_rank, max_rank) = (sys.float_info[0], sys.float_info[3])
+#
+#         for w in ws.values():
+#             if w < min_rank:
+#                 min_rank = w
+#             if w > max_rank:
+#                 max_rank = w
+#
+#         for n, w in ws.items():
+#             # to unify the weights, don't *100.
+#             ws[n] = (w - min_rank / 10.0) / (max_rank - min_rank / 10.0)
+#
+#         return ws
+#
+# class my_TextRank():
+#
+#     def __init__(self):
+#         self.tokenizer = self.postokenizer = jieba.posseg.dt
+#         self.pos_filt = frozenset(('ns', 'n', 'vn', 'v'))
+#         self.span = 5
+#
+#
+#     def textrank(self, sentence, topK=20, withWeight=False, allowPOS=('ns', 'n', 'vn', 'v'), withFlag=False):
+#         g = UndirectWeightedGraph()
+#         cm = defaultdict(int)
+#
+#         wordlist = segmentor(sentence)
+#         taglist = posttagger(wordlist)
+#
+#         words = zip(range(0,len(wordlist)),zip(wordlist, taglist))
+#         words = [w for w in words if w[1][1] in allowPOS]
+#
+#         for i, wp in enumerate(words):
+#             for j in xrange(i + 1, i + self.span):
+#                 if j >= len(words):  #快到末尾的时候，加了span可能溢出，忽略
+#                     break
+#                 else:
+#                     cm[(wp[1], words[j][1])] += 1
+#
+#
+#         for terms, w in cm.items():
+#             g.addEdge(terms[0], terms[1], w)
+#         nodes_rank = g.rank()
+#         if withWeight:
+#             tags = sorted(nodes_rank.items(), key=itemgetter(1), reverse=True)
+#         else:
+#             tags = sorted(nodes_rank, key=nodes_rank.__getitem__, reverse=True)
+#
+#         if topK:
+#             return tags[:topK]
+#         else:
+#             return tags
+#
+#     extract_tags = textrank
